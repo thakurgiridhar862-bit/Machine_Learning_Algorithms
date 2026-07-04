4d2f49958ab4ea59c2324fd9ae56f8205e4da8ce
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-04 16:45:53 +0530

    Add dataset loading and overview module

---
+++ Gradient_Descent/train.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+
+def load_data():
+    df = pd.read_csv("Gradient_Descent/data/student_marks_regression.csv")
+    return df
+
+
+def data_overview(df):
+    print("-" * 60)
+    print("DATASET OVERVIEW")
+    print("-" * 50)
+
+    print("\nFIRST FIVE ROWS")
+    print(df.head(5))
+
+    print(f"\nTotal row              : {df.shape[0]}")
+    print(f"Total Columns          : {df.shape[1]}")
+    print(f"Total Null Values      : {df.isnull().sum().sum()}")
+    print(f"Total Duplicate Values : {df.duplicated().sum()}")
+
+    print("\nMISSING VALUES ")
+    print("-" * 50)
+    print(df.isnull().sum())
+
+    print(" \n DATA TYPES")
+    print("-" * 50)
+    print(df.dtypes)
+
+    print("\nNUMERICAL SUMMARY")
+    print("-" * 50)
+    print(df.describe())
+
+
+def main():
+    df = load_data()
+    data_overview(df)
+
+
+if __name__ == "__main__":
+    main()
