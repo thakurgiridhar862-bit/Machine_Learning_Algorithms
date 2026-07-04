6c11e5eb9f408e4c5fe0834168ff73738f3e3eb6
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-04 16:51:38 +0530

    Add feature selection and train test split

--- Gradient_Descent/train.py
+++ Gradient_Descent/train.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 def load_data():
@@ -32,9 +33,40 @@ def data_overview(df):
     print(df.describe())
 
 
+def feature_selection(df):
+    X = df[["StudyHours"]]
+    Y = df["Marks"]
+
+    print("\nFEATURE SELECTION")
+    print("-" * 50)
+
+    print("\nInput Feature (X):")
+    print(X.columns.tolist())
+
+    print("\nTarget Variable (Y):")
+    print(Y.name)
+
+    return X, Y
+
+
+def tts(X, Y):
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=0.2, random_state=56
+    )
+    print("\nTRAIN TEST SPLIT OF DATA")
+    print("-" * 50)
+    print(f"Shape of X_train : {X_train.shape}")
+    print(f"Shape of X_test  : {X_test.shape}")
+    print(f"Shape of Y_train : {Y_train.shape}")
+    print(f"Shape of Y_test  : {Y_test.shape}")
+    return X_train, X_test, Y_train, Y_test
+
+
 def main():
     df = load_data()
     data_overview(df)
+    X, Y = feature_selection(df)
+    X_train, X_test, Y_train, Y_test = tts(X, Y)
 
 
 if __name__ == "__main__":
