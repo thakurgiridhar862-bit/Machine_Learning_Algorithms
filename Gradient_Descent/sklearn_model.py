33b9bc60905317b1a6d90a7b0d60ec76bf6d1dd3
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-13 16:22:55 +0530

    Slearn model

--- Gradient_Descent/sklearn_model.py
+++ Gradient_Descent/sklearn_model.py
@@ -7,18 +7,37 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 def main():
     df = load_data()
-
     df = clean_data(df)
+
     X_train, X_test, Y_train, Y_test = train_test(df)
 
-    lr = LinearRegression()
-    lr.fit(X_train, Y_train)
-    Y_pred = lr.predict(X_test)
+    X_train_2d = X_train.values.reshape(-1, 1)
+    X_test_2d = X_test.values.reshape(-1, 1)
 
-    comparision = pd.DataFrame(
+    lr = LinearRegression()
+
+    lr.fit(X_train_2d, Y_train)
+
+    Y_pred = lr.predict(X_test_2d)
+
+    comparison = pd.DataFrame(
         {
             "StudyHours": X_test.reset_index(drop=True),
-            "Marks": Y_test.reset_index(drop=True),
-            "Predicted Marks": Y_pred.reset_index(drop=True),
+            "Actual Marks": Y_test.reset_index(drop=True),
+            "Predicted Marks": Y_pred,
         }
     )
+
+    mae = mean_absolute_error(Y_test, Y_pred)
+    mse = mean_squared_error(Y_test, Y_pred)
+    rmse = mse**0.5
+    r2 = r2_score(Y_test, Y_pred)
+
+    print("\nFINAL MODEL VALUES")
+    print("-" * 50)
+    print(f"Slope     : {lr.coef_[0]}")
+    print(f"Intercept : {lr.intercept_}")
+
+    print("\nACTUAL VS PREDICTED")
+    print("-" * 50)
+    print(comparison.head(10))
