81a886c2727476a5144872ae3c5de4c772e4746a
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-13 15:29:49 +0530

    Train Test Split

--- Gradient_Descent/data_preprocessing.py
+++ Gradient_Descent/data_preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 def load_data():
@@ -18,9 +19,27 @@ def clean_data(df):
     return df
 
 
+def train_test(df):
+
+    X = df["StudyHours"]
+    Y = df["Marks"]
+
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, Y, test_size=0.2, random_state=56
+    )
+
+    print("\nAfter Splitting")
+    print("-" * 50)
+    print(f"X_Train Size = {X_train.shape}")
+    print(f"X_Test Size = {X_test.shape}")
+    print(f"Y_Train Size = {Y_train.shape}")
+    print(f"Y_Test Size = {Y_test.shape}")
+
+
 def main():
     df = load_data()
     df = clean_data(df)
+    train_test(df)
 
 
 if __name__ == "__main__":
