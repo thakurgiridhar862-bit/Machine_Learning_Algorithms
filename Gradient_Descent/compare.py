23939bb4f79556991560041deaa0f12f9655a421
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-13 16:32:48 +0530

    compare2

--- Gradient_Descent/compare.py
+++ Gradient_Descent/compare.py
@@ -1,4 +1,5 @@
 from data_preprocessing import load_data, clean_data, train_test
+from GD import GS_reg
 from sklearn.linear_model import LinearRegression
 
 
@@ -10,6 +11,22 @@ def main():
 
     X_train_2d = X_train.values.reshape(-1, 1)
     X_test_2d = X_test.values.reshape(-1, 1)
+
+    gs = GS_reg(0.00001, 1000)
+
+    gs.fit(X_train, Y_train)
+
+    Y_pred_GS = gs.predict(X_test)
+
+    lr = LinearRegression()
+
+    lr.fit(X_train_2d, Y_train)
+
+    Y_pred_lr = lr.predict(X_test_2d)
+
+    print("=" * 70)
+    print("SIMPLE LINEAR REGRESSION Vs GRADIENT DESCENT MODEL COMPARISON")
+    print("=" * 70)
 
 
 if __name__ == "__main__":
