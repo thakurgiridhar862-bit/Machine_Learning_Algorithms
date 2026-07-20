6455a569e1bd69347edb071e9fcb9753fea64a21
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-20 23:11:20 +0530

    Added Standard scalling

--- Gradient_Descent/Batch_GD_Ndim/data_preprocessing.py
+++ Gradient_Descent/Batch_GD_Ndim/data_preprocessing.py
@@ -1,29 +1,53 @@
 from data_overview import load_data
-import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 def tts(df):
 
-    X = df[["CGPA", "Projects", "DSA_Score", "Internships", "Communication_Score"]]
+    X = df[
+        [
+            "CGPA",
+            "Projects",
+            "DSA_Score",
+            "Internships",
+            "Communication_Score",
+        ]
+    ]
+
     Y = df["Package_LPA"]
 
     X_train, X_test, Y_train, Y_test = train_test_split(
-        X, Y, test_size=0.2, random_state=56
+        X,
+        Y,
+        test_size=0.2,
+        random_state=56,
     )
+
+    scaler = StandardScaler()
+
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
 
     print("\nAfter Splitting")
     print("-" * 50)
-    print(f"X_Train Size = {X_train.shape}")
-    print(f"X_Test Size = {X_test.shape}")
-    print(f"Y_Train Size = {Y_train.shape}")
-    print(f"Y_Test Size = {Y_test.shape}")
-    return X_train, X_test, Y_train, Y_test
+    print(f"X Train Size = {X_train.shape}")
+    print(f"X Test Size  = {X_test.shape}")
+    print(f"Y Train Size = {Y_train.shape}")
+    print(f"Y Test Size  = {Y_test.shape}")
+
+    print("\nAfter Feature Scaling")
+    print("-" * 50)
+    print(f"X Train Scaled Size = {X_train_scaled.shape}")
+    print(f"X Test Scaled Size  = {X_test_scaled.shape}")
+
+    return X_train_scaled, X_test_scaled, Y_train, Y_test, scaler
 
 
 def main():
     df = load_data()
-    X_train, X_test, Y_train, Y_test = tts(df)
+
+    X_train, X_test, Y_train, Y_test, scaler = tts(df)
 
 
 if __name__ == "__main__":
