f866b30f99bdd9ec0679568a26f6b432e37619f5
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-20 23:26:22 +0530

    Implement N-dimensional batch gradient descent

--- Gradient_Descent/Batch_GD_Ndim/Batch_GD.py
+++ Gradient_Descent/Batch_GD_Ndim/Batch_GD.py
@@ -12,4 +12,15 @@ class GS_Reg:
     def fit(self, X_train, Y_train):
 
         self.inter = 0
-        self.co = np.ones(X_train.shape[0])
+        self.co = np.ones(X_train.shape[1])
+
+        for i in range(self.ep):
+            y_hat = np.dot(X_train, self.co) + self.inter
+            in_der = -2 * np.mean(Y_train - y_hat)
+            self.inter -= self.lr * in_der
+
+            co_der = (-2 / X_train.shape[0]) * np.dot(X_train.T, Y_train - y_hat)
+            self.co -= self.lr * co_der
+
+    def predict(self, X_test):
+        return np.dot(X_test, self.co) + self.inter
