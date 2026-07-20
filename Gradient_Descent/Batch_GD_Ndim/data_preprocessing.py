6a2ceac4670bfe2b4335c43a5704cc7953285e01
thakurgiridhar862-bit <thakurgiridhar862@gmail.com>
2026-07-20 23:05:17 +0530

    Added preprocessing file

---
+++ Gradient_Descent/Batch_GD_Ndim/data_preprocessing.py
@@ -0,0 +1,11 @@
+from data_overview import load_data
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+
+def main():
+    df = load_data()
+
+
+if __name__ == "__main__":
+    main()
